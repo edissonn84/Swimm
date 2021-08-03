73665a66eec2398095a90e5f779158f3b9cf3290
edissonn84 <edissonn@mail.ru>
2021-08-04 00:00:07 +0300

    First deploy

---
+++ src/main/java/com/example/projectred/ProjectREdApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectREdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectREdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectred/ProjectREdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectREdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
